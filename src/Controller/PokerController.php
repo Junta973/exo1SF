d42c1dc3a2dc0bc49467cb77c09905fd9e79b2fb
medard <marc.medard973@gmail.com>
2021-07-02 09:12:50 +0200

    exercice de redirection

--- src/Controller/PokerController.php
+++ src/Controller/PokerController.php
@@ -4,11 +4,14 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PokerController
+class PokerController extends AbstractController
+
+
 {
     /**
      * @Route("/poker", name="poker")
@@ -19,12 +22,32 @@ class PokerController
         $age = $request->query->get('age');
 
         if ($age >= 18) {
-            return new Response('vous êtes autorisé à vous connecter');
+            return $this->redirectToRoute('digimon');
         } else {
-            return new Response('Vous n\'êtes pas autorisé à vous connecter');
+            return $this->redirectToRoute('amokila');
+        }
+    }
+
+    /**
+     * @Route("/digimon", name="digimon")
+     */
+    public function digimon()
+    {
+        $request = Request::createFromGlobals();
+        {
+            return $this->render('digimon.html.twig');
         }
     }
 
 
-
+    /**
+     * @Route("/amokila", name="amokila")
+     */
+    public function amokila()
+    {
+        $request = Request::createFromGlobals();
+       {
+           return $this->render('amokila.html.twig');
+        }
+    }
 }
