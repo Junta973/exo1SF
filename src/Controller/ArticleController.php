49e01f516e511fa166fedf00d5ac36c1d7348d35
medard <marc.medard973@gmail.com>
2021-07-02 11:43:53 +0200

    renvoyer le titre en fonction de l'id

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -18,12 +18,38 @@ class ArticleController
 
 
     //utilisation de la méthode wildcard
+    //renvoi le titre de l'id concerné
     /**
      * @Route("/articles/{id}", name="articleShow")
      */
     public function articleShhow($id)
     {
+        $articles = [
+            1 => [
+                "title" => "La vaccination c'est trop géniale",
+                "content" => "bablablblalba",
+                "id" => 1
+            ],
+            2 => [
+                "title" => "La vaccination c'est pas trop géniale",
+                "content" => "blablablabla",
+                "id" => 2
+            ],
+            3 => [
+                "title" => "Balkany c'est trop génial",
+                "content" => "balblalblalb",
+                "id" => 3
+            ],
+            4 => [
+                "title" => "Balkany c'est pas trop génial",
+                "content" => "balblalblalb",
+                "id" => 4
+            ]
+        ];
 
-        return new Response("<h3>L'id de la page actuelle est : $id</h3>");
+        $article = $articles[$id];
+        return new Response($article['title']);
     }
+
+
 }
